72c7afd0e82dc5ba0477d7e5dd1d9e83c238ec73
luiseyer <luiseyer2001@proton.me>
2025-07-10 15:08:50 -0400

    feat(getExpirationDate)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+import { DEFAULT_EXPIRATION_DAYS } from '$lib/consts'
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
@@ -14,4 +15,14 @@ export type WithoutChildren<T> = T extends { children?: any }
 export type WithoutChildrenOrChild<T> = WithoutChildren<WithoutChild<T>>
 export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & {
   ref?: U | null
+}
+
+export function getExpirationDate(
+  days: number = DEFAULT_EXPIRATION_DAYS
+): Date {
+  const currentDate = new Date()
+  const expirationDate = new Date(currentDate)
+
+  expirationDate.setDate(currentDate.getDate() + days)
+  return expirationDate
 }
