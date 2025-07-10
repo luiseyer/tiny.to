34541b2ad60b800c77bd909ae9ab0ec7fe8b2ceb
luiseyer <luiseyer2001@proton.me>
2025-07-10 15:13:18 -0400

    refactor: add fonts and auth context

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
   import '../app.css'
+  import '@fontsource-variable/inter'
+  import '@fontsource-variable/jetbrains-mono'
+  import { setAuthContext } from '$lib/auth.svelte'
   import { Button } from '$lib/components/ui/button'
   import MoonIcon from '@lucide/svelte/icons/moon'
   import SunIcon from '@lucide/svelte/icons/sun'
   import { ModeWatcher, toggleMode } from 'mode-watcher'
 
   let { children } = $props()
+
+  setAuthContext()
 </script>
 
-<Button onclick={toggleMode} variant="outline" size="icon" class="fixed right-5 bottom-5">
+<ModeWatcher />
+
+{@render children?.()}
+
+<Button onclick={toggleMode} variant="secondary" size="icon" class="fixed right-5 bottom-5">
   <SunIcon
     class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90"
   />
@@ -17,6 +26,3 @@
   />
   <span class="sr-only">Toggle theme</span>
 </Button>
-
-<ModeWatcher />
-{@render children?.()}
