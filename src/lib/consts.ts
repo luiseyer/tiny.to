e943e934dcece2b040e9652e74a3fbb846bdd1de
luiseyer <luiseyer2001@proton.me>
2025-07-14 15:25:37 -0400

    refactor(consts): add Routes

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,1 +1,13 @@
 export const DEFAULT_EXPIRATION_DAYS = 7
+
+export const Routes = {
+  Home: '/',
+  Login: '/login',
+  Register: '/register',
+  Logout: '/logout',
+
+  Dashboard: '/dashboard',
+  Links: '/links',
+  Analytics: '/analytics',
+  Settings: '/settings'
+} as const
