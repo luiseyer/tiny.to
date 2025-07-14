d9c331e65bb0d48ba7333721a52d04d730af8241
luiseyer <luiseyer2001@proton.me>
2025-07-14 15:43:05 -0400

    feat(date-picker)

---
+++ src/lib/components/date-picker.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+  import { buttonVariants } from '$lib/components/ui/button'
+  import { Calendar } from '$lib/components/ui/calendar'
+  import * as Popover from '$lib/components/ui/popover'
+  import * as Select from '$lib/components/ui/select'
+  import { cn } from '$lib/utils'
+  import {
+    CalendarDate,
+    DateFormatter,
+    type DateValue,
+    getLocalTimeZone,
+    today,
+  } from '@internationalized/date'
+  import CalendarIcon from '@lucide/svelte/icons/calendar'
+
+  const df = new DateFormatter('es', {
+    dateStyle: 'long',
+  })
+
+  const parse = (value: string | undefined) => {
+    try {
+      if (!value) return undefined
+
+      const date = new Date(value)
+
+      return new CalendarDate(date.getFullYear(), date.getMonth() + 1, date.getDate())
+    } catch (e) {
+      console.log(e)
+      return undefined
+    }
+  }
+
+  let { value = $bindable() }: { value: string | undefined } = $props()
+
+  let valueDate: DateValue | undefined = $state(parse(value))
+  const valueString = $derived(
+    valueDate ? df.format(valueDate.toDate(getLocalTimeZone())) : 'Nunca Expira'
+  )
+
+  const items = [
+    { value: 1, label: 'Mañana' },
+    { value: 3, label: 'En 3 días' },
+    { value: 7, label: 'En 7 días' },
+    { value: 30, label: 'En 30 días' },
+    { value: 0, label: 'Nunca Expira' },
+  ]
+</script>
+
+<Popover.Root>
+  <Popover.Trigger
+    class={cn(
+      buttonVariants({
+        variant: 'outline',
+        class: 'justify-start text-left font-normal',
+      }),
+      { 'text-muted-foreground': !valueDate }
+    )}
+  >
+    <CalendarIcon class="mr-2 size-4" />
+    {valueDate ? df.format(valueDate.toDate(getLocalTimeZone())) : 'Selecciona una fecha'}
+  </Popover.Trigger>
+  <Popover.Content class="flex w-auto flex-col space-y-2 p-2">
+    <Select.Root
+      type="single"
+      bind:value={
+        () => valueString,
+        (v) => {
+          if (!v || Number.parseInt(v) === 0) {
+            valueDate = undefined
+            return
+          }
+          valueDate = today(getLocalTimeZone()).add({ days: Number.parseInt(v) })
+        }
+      }
+    >
+      <Select.Trigger>
+        {valueString}
+      </Select.Trigger>
+      <Select.Content>
+        {#each items as item (item.value)}
+          <Select.Item value={`${item.value}`}>{item.label}</Select.Item>
+        {/each}
+      </Select.Content>
+    </Select.Root>
+    <div class="rounded-md border">
+      <Calendar
+        type="single"
+        bind:value={valueDate as DateValue}
+        onValueChange={(v) => {
+          if (!v) return
+          value = v.toDate(getLocalTimeZone()).toISOString()
+        }}
+      />
+    </div>
+  </Popover.Content>
+</Popover.Root>
