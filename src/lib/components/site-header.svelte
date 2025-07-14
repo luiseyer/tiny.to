3bb778eb9591c882cccc34ac81214ca0351f446c
luiseyer <luiseyer2001@proton.me>
2025-07-14 15:28:24 -0400

    refactor: replace TitleState with svelte's page state

--- src/lib/components/site-header.svelte
+++ src/lib/components/site-header.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
+  import { page } from '$app/state'
+  import ToggleTheme from '$lib/components/toggle-theme.svelte'
   import { Separator, SidebarTrigger } from '$lib/components/ui/sidebar'
-  import { getTitleState } from '$lib/title.svelte'
-
-  const title = getTitleState()
 </script>
 
 <header
-  class="flex h-12 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12"
+  class="flex h-12 shrink-0 items-center gap-2 border-b px-4 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 lg:px-6"
 >
-  <div class="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+  <div class="flex w-full items-center gap-1 lg:gap-2">
     <SidebarTrigger class="-ml-1" />
     <Separator orientation="vertical" class="mx-2 data-[orientation=vertical]:h-4" />
-    <h1 class="text-base font-medium">{title.value}</h1>
+    <h1 class="text-base font-medium">{page.data.title}</h1>
+  </div>
+
+  <div class="flex items-center gap-1 lg:gap-2">
+    <ToggleTheme />
   </div>
 </header>
