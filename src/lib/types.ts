37a2b76ff94e997cfc6c8b23ec37cc4420d90e75
luiseyer <luiseyer2001@proton.me>
2025-07-14 15:46:43 -0400

    refactor: update types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,10 @@
-import type { click, link, user } from '$lib/schemas'
+import type { click, link, linkCreate, linkUpdate, user } from '$lib/schemas'
 import type { z } from 'zod/v4'
 
 export type User = z.infer<typeof user>
+
 export type Link = z.infer<typeof link>
+export type LinkCreate = z.infer<typeof linkCreate>
+export type LinkUpdate = z.infer<typeof linkUpdate>
+
 export type Click = z.infer<typeof click>
