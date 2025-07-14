6f96316e518d0152efcc545008024af15d4d0bc6
luiseyer <luiseyer2001@proton.me>
2025-07-14 15:49:56 -0400

    refactor: add initial user data

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,27 +2,22 @@
   import '../app.css'
   import '@fontsource-variable/inter'
   import '@fontsource-variable/jetbrains-mono'
-  import { setAuthContext } from '$lib/auth.svelte'
-  import { Button } from '$lib/components/ui/button'
-  import MoonIcon from '@lucide/svelte/icons/moon'
-  import SunIcon from '@lucide/svelte/icons/sun'
-  import { ModeWatcher, toggleMode } from 'mode-watcher'
+  import { setAuthState } from '$lib/auth'
+  import { Toaster } from '$lib/components/ui/sonner'
+  import { ModeWatcher, mode } from 'mode-watcher'
+  import type { LayoutProps } from './$types'
 
-  let { children } = $props()
+  let { children, data }: LayoutProps = $props()
 
-  setAuthContext()
+  setAuthState(data.user)
 </script>
 
+<svelte:head>
+  <title>Tiny.to</title>
+  <meta name="description" content="Tiny.to es un acortador de enlaces." />
+</svelte:head>
+
 <ModeWatcher />
+<Toaster richColors theme={mode.current ?? 'light'} />
 
 {@render children?.()}
-
-<Button onclick={toggleMode} variant="secondary" size="icon" class="fixed right-5 bottom-5">
-  <SunIcon
-    class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90"
-  />
-  <MoonIcon
-    class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 !transition-all dark:scale-100 dark:rotate-0"
-  />
-  <span class="sr-only">Toggle theme</span>
-</Button>
