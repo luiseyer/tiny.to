c711520ef4cc557bee45946134f8863b43b6377b
luiseyer <luiseyer2001@proton.me>
2025-07-14 15:47:20 -0400

    feat(links page)

---
+++ src/routes/(app)/links/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import DataTable from '$lib/links/components/data-table'
+  import { Modal, setModalState } from '$lib/links/components/modal'
+
+  setModalState()
+</script>
+
+<Modal />
+
+<div class="flex flex-col gap-4 px-4 lg:px-6">
+  <DataTable />
+</div>
