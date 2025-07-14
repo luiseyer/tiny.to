21e23740e06983fab97e53c375d3f636d69089b1
luiseyer <luiseyer2001@proton.me>
2025-07-14 15:38:15 -0400

    refactor: improve hooks.server
    - extract PocketBase logic in `authentication` function
    - implement redirects in `authtorization` function
    - export `handle` function as `authentication -> authtorization` sequence

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,10 @@
-import { building } from '$app/environment'
+import { Routes } from '$lib/consts'
+import { user } from '$lib/schemas'
 import { PocketBase } from '$lib/shared/pocketbase'
+import { type Handle, redirect } from '@sveltejs/kit'
+import { sequence } from '@sveltejs/kit/hooks'
 
-export async function handle({ event, resolve }) {
+export const authentication: Handle = async ({ event, resolve }) => {
   event.locals.pb = new PocketBase()
 
   event.locals.pb.authStore.loadFromCookie(
@@ -9,21 +12,42 @@ export async function handle({ event, resolve }) {
   )
 
   try {
-    event.locals.pb.authStore.isValid &&
-      (await event.locals.pb.users.authRefresh())
+    if (event.locals.pb.authStore.isValid) {
+      await event.locals.pb.users.authRefresh()
+      event.locals.user = user.safeParse(event.locals.pb.authStore.record).data
+    }
   } catch (_) {
     event.locals.pb.authStore.clear()
   }
 
   const response = await resolve(event)
 
-  response.headers.append(
-    'set-cookie',
-    event.locals.pb.authStore.exportToCookie({
-      httpOnly: false,
-      secure: building,
-    })
-  )
+  const pbCookie = event.locals.pb.authStore.exportToCookie({
+    httpOnly: false,
+    sameSite: 'lax'
+  })
+
+  response.headers.append('set-cookie', pbCookie)
 
   return response
 }
+
+const UNPROTECTED_PATHS = [Routes.Login, Routes.Register, Routes.Logout]
+
+export const authtorization: Handle = async ({ event, resolve }) => {
+  const { pathname } = event.url
+
+  const isProtectedPath =
+    !UNPROTECTED_PATHS.some((path) => pathname.startsWith(path)) &&
+    pathname !== Routes.Home
+
+  const isAuthenticated = event.locals.pb.authStore.isValid
+
+  if (isProtectedPath && !isAuthenticated) {
+    return redirect(302, Routes.Login)
+  }
+
+  return await resolve(event)
+}
+
+export const handle = sequence(authentication, authtorization)
