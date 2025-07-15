d13c49fbe9e1f41906c1dfa9aa3ef20186384089
luiseyer <luiseyer2001@proton.me>
2025-07-15 12:05:58 -0400

    refactor: get only data created by current user

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,8 +1,14 @@
 import type { ServerLoad } from '@sveltejs/kit'
 
 export const load: ServerLoad = async ({ locals }) => {
+  const { id } = locals.user || {}
+
   return {
-    links: await locals.pb.links.getFullList(),
-    clicks: await locals.pb.clicks.getFullList()
+    links: id
+      ? await locals.pb.links.getFullList({ filter: `user="${id}"` })
+      : [],
+    clicks: id
+      ? await locals.pb.clicks.getFullList({ filter: `link.user="${id}"` })
+      : []
   }
 }
